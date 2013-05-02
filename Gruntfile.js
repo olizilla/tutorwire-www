85f290af65b4c164453bd14971c46046027f2eea
olizilla <oli@zilla.org.uk>
2013-05-02 09:54:50 +0100

    WIP: ideas on externalising templates from the Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,63 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    
+    pkg: grunt.file.readJSON('package.json'),
+    
+    banner: grunt.template.process(grunt.file.read('html/banner.src.html')),
+
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist: {
+        src: ['lib/<%= pkg.name %>.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {}
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib_test: {
+        src: ['lib/**/*.js', 'test/**/*.js']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint','concat', 'uglify']);
+
+};
